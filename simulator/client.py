44b546a1348df90e57d3711b51296e82733f8c30
Subrose <team@subrose.io>
2023-06-25 19:42:18 +0100

    updates

--- simulator/client.py
+++ simulator/client.py
@@ -1,6 +1,6 @@
 """ Client with helpers for similating vault usecases """
 
-from typing import Tuple, Optional, Dict, Any, List
+from typing import Any, Optional, List
 import requests
 
 VAULT_URL = "http://localhost:3001"
@@ -17,6 +17,21 @@ def auth_guard(fn):
     return auth_check
 
 
+def check_expected_status(
+    response: requests.Response, expected_status_codes: Optional[list[int]]
+):
+    if expected_status_codes is None:
+        return
+    if response.status_code not in expected_status_codes:
+        raise Exception(
+            f"""Request failed:
+            path={response.request.path_url}, body={response.request.body}
+            {response.status_code} not in {expected_status_codes=} 
+            {response.reason}, {response.json()}
+            """
+        )
+
+
 class Actor:
     def __init__(self, vault_url: str, name: str, access_key: str, secret_key: str):
         self.name = name
@@ -25,127 +40,91 @@ class Actor:
         self.vault_url = vault_url
         self.token = None
 
-    def authenticate(self) -> Tuple[Optional[bool], int, Optional[str]]:
+    def authenticate(
+        self, expected_statuses: Optional[list[int]] = None
+    ) -> dict[str, str]:
         response = requests.post(
             f"{self.vault_url}/auth/token",
             auth=(self.access_key, self.secret_key),
         )
-        if response.status_code == 200:
-            self.token = response.json()["access_token"]
-            return True, response.status_code, None
-        return None, response.status_code, "Authentication failed"
+        check_expected_status(response, expected_statuses)
+        self.token = response.json()["access_token"]
+        return response.json()
 
     @auth_guard
     def create_principal(
-        self, name: str, description: str, policies: List[str]
-    ) -> Tuple[Optional[Dict[str, Any]], int, Optional[str]]:
+        self,
+        name: str,
+        description: str,
+        policies: List[str],
+        expected_statuses: Optional[list[int]] = None,
+    ) -> dict[str, str]:
         response = requests.post(
             f"{self.vault_url}/principals",
             json={"name": name, "description": description, "policies": policies},
             headers={"Authorization": f"Bearer {self.token}"},
         )
-        if response.status_code == 201:
-            return response.json(), response.status_code, None
-        return None, response.status_code, "Create principal failed"
+        check_expected_status(response, expected_statuses)
+        return response.json()
 
     @auth_guard
     def create_collection(
-        self, schema: Dict[str, Any]
-    ) -> Tuple[Optional[bool], int, Optional[str]]:
+        self, schema: dict[str, Any], expected_statuses: Optional[list[int]] = None
+    ) -> None:
         response = requests.post(
             f"{self.vault_url}/collections",
             json=schema,
             headers={"Authorization": f"Bearer {self.token}"},
         )
-        if response.status_code == 201:
-            return True, response.status_code, None
-        return None, response.status_code, response.json()
+        check_expected_status(response, expected_statuses)
 
     @auth_guard
     def create_records(
-        self, collection: str, record: List[Dict[str, Any]]
-    ) -> Tuple[Optional[List[str]], int, Optional[str]]:
+        self,
+        collection: str,
+        records: List[dict[str, str]],
+        expected_statuses: Optional[list[int]] = None,
+    ) -> list[str]:
         response = requests.post(
             f"{self.vault_url}/collections/{collection}/records",
-            json=record,
+            json=records,
             headers={"Authorization": f"Bearer {self.token}"},
         )
-        if response.status_code == 201:
-            return response.json(), response.status_code, None
-        return None, response.status_code, "Create record failed"
+        check_expected_status(response, expected_statuses)
+        return response.json()
 
     @auth_guard
     def get_record(
-        self, collection: str, record_id: str
-    ) -> Tuple[Optional[Dict[str, Any]], int, Optional[str]]:
+        self,
+        collection: str,
+        record_id: str,
+        expected_statuses: Optional[list[int]] = None,
+    ) -> dict[str, str]:
         response = requests.get(
             f"{self.vault_url}/collections/{collection}/records/{record_id}",
             headers={"Authorization": f"Bearer {self.token}"},
         )
-        if response.status_code == 200:
-            return response.json(), response.status_code, None
-        return None, response.status_code, "Get record failed"
+        check_expected_status(response, expected_statuses)
+        return response.json()
 
     @auth_guard
     def create_policy(
-        self, policy: Dict[str, Any]
-    ) -> Tuple[Optional[Dict[str, Any]], int, Optional[str]]:
+        self, policy: dict[str, str], expected_statuses: Optional[list[int]] = None
+    ) -> None:
         response = requests.post(
             f"{self.vault_url}/policies",
             headers={"Authorization": f"Bearer {self.token}"},
             json=policy,
         )
-        if response.status_code == 201:
-            return response.json(), response.status_code, None
-        return None, response.status_code, "Create policy failed"
+        check_expected_status(response, expected_statuses)
 
     @auth_guard
     def get_policy(
-        self, policy_id: str
-    ) -> Tuple[Optional[Dict[str, Any]], int, Optional[str]]:
+        self, policy_id: str, expected_statuses: Optional[list[int]] = None
+    ) -> None:
         response = requests.get(
             f"{self.vault_url}/policies/{policy_id}",
             headers={"Authorization": f"Bearer {self.token}"},
         )
-        if response.status_code == 200:
-            return response.json(), response.status_code, None
-        return None, response.status_code, "Get policy failed"
-
-
-if __name__ == "__main__":
-    admin = Actor(VAULT_URL, "admin", "admin", "admin")
-    admin.authenticate()
-
-    res = admin.create_collection(
-        {
-            "name": "users",
-            "fields": {
-                "fname": {"type": "string", "indexed": True},
-                "lname": {"type": "string", "indexed": True},
-            },
-        }
-    )
-    rec = admin.create_records(
-        "users",
-        [
-            {"fname": "John", "lname": "Doe"},
-            {"fname": "Alex", "lname": "Doe"},
-            {"fname": "Mike", "lname": "Doe"},
-        ],
-    )
-    pp = admin.create_policy(
-        policy={
-            "policy_id": "alice-read-users-records",
-            "effect": "allow",
-            "action": "read",
-            "resource": "collections/users/records/",
-        }
-    )
-    pp = admin.create_policy(
-        policy={
-            "policy_id": "alice-read-users-collection",
-            "effect": "allow",
-            "action": "read",
-            "resource": "collections/users/",
-        }
-    )
+        check_expected_status(response, expected_statuses)
+        return response.json()
