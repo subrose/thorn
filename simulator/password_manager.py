7ee24d77d0b0d1421f7ede372b2221bdbfc2831c
subroseio <136270129+subroseio@users.noreply.github.com>
2023-07-05 13:08:41 +0100

    Use-case enhancements  (#13)
    * updates
    
    * pwd manager complete
    
    * removing print
    
    * tests passing (#15)
    
    * usecase working with new paths

--- simulator/password_manager.py
+++ simulator/password_manager.py
@@ -4,137 +4,147 @@ from client import Actor
 VAULT_URL = "http://localhost:3001"
 
 # Step 0: Initialize your actors
-admin = Actor(VAULT_URL, "admin", "admin", "admin")
-assert admin.authenticate()[0] is True
+admin = Actor(VAULT_URL, name="admin", access_key="admin", secret_key="admin")
+admin.authenticate(expected_statuses=[200])
 
 # Step 2: Create collection
-_, status_code, error = admin.create_collection(
-    {
+admin.create_collection(
+    schema={
         "name": "alice-passwords",
         "fields": {
             "service": {"type": "string", "indexed": False},
             "password": {"type": "string", "indexed": False},
         },
-    }
+    },
+    expected_statuses=[201, 409],
 )
-if status_code not in [201, 409]:
-    raise Exception(f"Failed to create collection: {status_code}, {error}")
 
-_, status_code, error = admin.create_collection(
-    {
+admin.create_collection(
+    schema={
         "name": "bob-passwords",
         "fields": {
             "service": {"type": "string", "indexed": False},
             "password": {"type": "string", "indexed": False},
         },
-    }
+    },
+    expected_statuses=[201, 409],
 )
-
-if status_code not in [201, 409]:
-    raise Exception(f"Failed to create collection: {status_code}, {error}")
 
 
 # Step 3: Create policies using admin role
-_, status_code, error = admin.create_policy(
+admin.create_policy(
     policy={
         "policy_id": "alice-read-own-passwords",
         "effect": "allow",
         "action": "read",
-        "resource": "collections/alice-passwords/*",
-    }
+        "resource": "/collections/alice-passwords/*",
+    },
+    expected_statuses=[201, 409],
 )
-if error:
-    raise Exception(f"Failed to create policy: {error}")
 
-_, status_code, error = admin.create_policy(
+admin.create_policy(
     policy={
         "policy_id": "alice-write-own-passwords",
         "effect": "allow",
         "action": "write",
-        "resource": "collections/alice-passwords/*",
-    }
+        "resource": "/collections/alice-passwords/*",
+    },
+    expected_statuses=[201, 409],
 )
-if error:
-    raise Exception(f"Failed to create policy: {error}")
 
-_, status_code, error = admin.create_policy(
+
+admin.create_policy(
     policy={
         "policy_id": "bob-read-own-passwords",
         "effect": "allow",
         "action": "read",
-        "resource": "collections/bob-passwords/*",
-    }
+        "resource": "/collections/bob-passwords/*",
+    },
+    expected_statuses=[201, 409],
 )
-if error:
-    raise Exception(f"Failed to create policy: {error}")
 
-_, status_code, error = admin.create_policy(
+admin.create_policy(
     policy={
         "policy_id": "bob-write-own-passwords",
         "effect": "allow",
         "action": "write",
-        "resource": "collections/bob-passwords/*",
-    }
-)
-if error:
-    raise Exception(f"Failed to create policy: {error}")
-
-# 1) Change record resource from collections to records
-# 2) Allow multiple actions and resources
-
-alice_res, status_code, error = admin.create_principal(
-    "alice", "alice", ["alice-read-own-passwords", "alice-write-own-passwords"]
+        "resource": "/collections/bob-passwords/*",
+    },
+    expected_statuses=[201, 409],
 )
 
-if error or alice_res is None:
-    raise Exception("Failed to create alice: ", error)
+alice_res = admin.create_principal(
+    "alice",
+    "alice",
+    ["alice-read-own-passwords", "alice-write-own-passwords"],
+    expected_statuses=[201, 409],
+)
+
+assert alice_res is not None
 
 alice = Actor(VAULT_URL, "alice", alice_res["access_key"], alice_res["access_secret"])
-alice.authenticate()
+alice.authenticate(expected_statuses=[200])
 
-bob_res, status_code, error = admin.create_principal(
+bob_res = admin.create_principal(
     "bob", "bob", ["bob-read-own-passwords", "bob-write-own-passwords"]
 )
-
-if error or bob_res is None:
-    raise Exception("Failed to create bob: ", error)
+assert bob_res is not None
 
 bob = Actor(VAULT_URL, "bob", bob_res["access_key"], bob_res["access_secret"])
-bob.authenticate()
+bob.authenticate(expected_statuses=[200])
 
 # 2) Alice adds a password
-alice_password_res, status_code, error = alice.create_records(
-    "alice-passwords", [{"service": "email", "password": "alice_password_1"}]
+alice_password = "alicerocks"
+alice_password_res = alice.create_records(
+    "alice-passwords",
+    [{"service": "email", "password": alice_password}],
+    expected_statuses=[201],
 )
-if error or alice_password_res is None:
-    raise Exception("Failed to create password for Alice: ", error)
 
 # 4) Bob adds a password
-bob_password_res, status_code, error = bob.create_records(
-    "bob-passwords", [{"service": "email", "password": "bob_password_1"}]
+bob_password = "bobisthebest"
+bob_password_res = bob.create_records(
+    "bob-passwords",
+    [{"service": "email", "password": bob_password}],
+    expected_statuses=[201],
 )
-if error or bob_password_res is None:
-    raise Exception("Failed to create password for Bob: ", error)
 
 alice_password_id = alice_password_res[0]
 bob_password_id = bob_password_res[0]
 
 # 5) Alice views her passwords
-_, status_code, error = alice.get_record("alice-passwords", alice_password_id)
-if error:
-    raise Exception("Failed to get password for Alice: " + error)
+alice_retrieved_password = alice.get_record(
+    collection="alice-passwords",
+    record_id=alice_password_id,
+    fields="service.plain,password.plain",
+    expected_statuses=[200],
+)
+
+assert alice_retrieved_password[alice_password_id]["password"] == alice_password
 
 # 6) Bob views his passwords
-_, status_code, error = bob.get_record("bob-passwords", bob_password_id)
-if error:
-    raise Exception("Failed to get password for Bob: " + error)
+bob_retrieved_password = bob.get_record(
+    collection="bob-passwords",
+    record_id=bob_password_id,
+    expected_statuses=[200],
+    fields="service.plain,password.plain",
+)
+assert bob_retrieved_password[bob_password_id]["password"] == bob_password
 
 # 7) Alice can't CRUD Bob's passwords
-_, status_code, error = alice.get_record("bob-passwords", bob_password_id)
-if not error:
-    raise Exception("Alice should not be able to access Bob's passwords")
+alice.get_record(
+    collection="bob-passwords",
+    record_id=bob_password_id,
+    fields="service.plain,password.plain",
+    expected_statuses=[403],
+)
 
 # 8) Bob can't CRUD Alice's passwords
-_, status_code, error = bob.get_record("alice-passwords", alice_password_id)
-if not error:
-    raise Exception("Bob should not be able to access Alice's passwords")
+bob.get_record(
+    collection="alice-passwords",
+    record_id=alice_password_id,
+    fields="service.plain,password.plain",
+    expected_statuses=[403],
+)
+
+print("Password manager usecase ok!")
