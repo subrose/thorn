8872e1503d022281be4aed86391d370efe99d1f9
Subrose <team@subrose.io>
2023-12-26 10:46:28 +0300

    more opts

--- vault/aes_privatiser.go
+++ vault/aes_privatiser.go
@@ -8,26 +8,25 @@ import (
 )
 
 type AESPrivatiser struct {
-	secret string
+	secret []byte
+	block  cipher.Block // Store the cipher block
 }
 
 func NewAESPrivatiser(secret string) (*AESPrivatiser, error) {
 	if len(secret) != 16 && len(secret) != 24 && len(secret) != 32 {
+		// TODO: Enforce AES-256
 		return nil, errors.New("invalid secret length; must be 16, 24, or 32 bytes for AES-128, AES-192, AES-256 respectively")
 	}
-	return &AESPrivatiser{secret}, nil
+	block, err := aes.NewCipher([]byte(secret))
+	if err != nil {
+		return nil, err
+	}
+	return &AESPrivatiser{[]byte(secret), block}, nil
 }
 
 func (p *AESPrivatiser) Encrypt(text string) (string, error) {
-	block, err := aes.NewCipher([]byte(p.secret))
-	if err != nil {
-		return "", err
-	}
-
-	// Using a fixed IV for deterministic encryption
-	iv := make([]byte, aes.BlockSize) // Fixed IV (e.g., all zeros)
-
-	cfb := cipher.NewCFBEncrypter(block, iv)
+	iv := make([]byte, aes.BlockSize)
+	cfb := cipher.NewCFBEncrypter(p.block, iv)
 	cipherText := make([]byte, len(text))
 	cfb.XORKeyStream(cipherText, []byte(text))
 
@@ -35,19 +34,14 @@ func (p *AESPrivatiser) Encrypt(text string) (string, error) {
 }
 
 func (p *AESPrivatiser) Decrypt(encodedText string) (string, error) {
-	block, err := aes.NewCipher([]byte(p.secret))
-	if err != nil {
-		return "", err
-	}
-
 	data, err := base64.StdEncoding.DecodeString(encodedText)
 	if err != nil {
 		return "", err
 	}
 
-	iv := make([]byte, aes.BlockSize) // Same fixed IV as used in encryption
+	iv := make([]byte, aes.BlockSize)
 
-	cfb := cipher.NewCFBDecrypter(block, iv)
+	cfb := cipher.NewCFBDecrypter(p.block, iv)
 	plainText := make([]byte, len(data))
 	cfb.XORKeyStream(plainText, data)
 
